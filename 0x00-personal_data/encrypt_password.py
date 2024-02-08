11e7ecc4a937b7adcdbd6473df8c1d15929203b9
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-08 15:05:57 +0300

    encrypt password using bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,9 +4,10 @@ a function that expects one string argument
 and returns a salted, hashed password,
 which is a byte string
 """
-import bcrypt
+# import bcrypt
+from bcrypt import hashpw, gensalt
 
 
 def hash_password(password: str) -> bytes:
     """encrypts password using bcrypt"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashpw(password.encode('utf-8'), gensalt())
