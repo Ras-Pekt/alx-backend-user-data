b6610bd2f5b92fb92d4134031e900c30ea7174d4
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-08 15:13:23 +0300

    validate provided password matches hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,10 +4,14 @@ a function that expects one string argument
 and returns a salted, hashed password,
 which is a byte string
 """
-# import bcrypt
-from bcrypt import hashpw, gensalt
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """encrypts password using bcrypt"""
-    return hashpw(password.encode('utf-8'), gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
