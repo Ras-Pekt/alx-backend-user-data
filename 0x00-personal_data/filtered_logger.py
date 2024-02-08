e1cc9bd05c606016317d8c41fc02ec5d5f79f596
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-08 14:12:51 +0300

    connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -59,7 +59,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-    db_conn = mysql.connector.connect(
+    db_conn = mysql.connector.connection.MySQLConnection(
         user=db_username,
         password=db_password,
         host=db_host,
