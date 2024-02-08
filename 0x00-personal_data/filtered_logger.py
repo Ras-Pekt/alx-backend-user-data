299bac181600a40ff7e8521e4b0aac61bea89c92
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-08 13:01:41 +0300

    format logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,3 +14,25 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f"{field}=.*?{separator}",
                          f"{field}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filters values in incoming log records"""
+        record_dict = filter_datum(
+                self.fields, self.REDACTION,
+                record.getMessage(), self.SEPARATOR
+            )
+        record.msg = record_dict
+        return super(RedactingFormatter, self).format(record)
