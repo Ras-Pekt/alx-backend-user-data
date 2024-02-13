325a7c760b8c87be74891fcb971803f941d36ab3
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-13 20:14:31 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python3
-"""Auth module for the API"""
-import fnmatch
-from flask import request
+"""This module defines class Auth"""
 from typing import List, TypeVar
+from flask import request
+import fnmatch
 
 
 class Auth:
-    """
-    template for all authentication system to be implemented
-    """
+    """Class responsible for handling Authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """handles paths that require authorization"""
-        if path is None or excluded_paths is None or excluded_paths == []:
+        """
+        return: True if authentication is required, False otherwise.
+        """
+        if path is None:
             return True
-
-        if path in excluded_paths or \
-            path[:-1] in excluded_paths or \
-                f"{path}/" in excluded_paths:
-            return False
-
+        if excluded_paths is None or not excluded_paths:
+            return True
+        normalized_path = path.rstrip('/') + '/'
         for excluded_path in excluded_paths:
-            if fnmatch(path, excluded_path):
+            if fnmatch.fnmatch(normalized_path, excluded_path):
                 return False
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """validates requests"""
-        if request is None:
+        """
+        return: Authorization header value.
+        """
+        if request is None or 'Authorization' not in request.headers:
             return None
-        return request.headers.get("Authorization")
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        return: Current user.
+        """
         return None
