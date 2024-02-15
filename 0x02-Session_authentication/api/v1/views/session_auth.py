08aa9f448429fef40dbe73285de54f879073f943
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-15 20:55:34 +0300

    new view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -12,24 +12,24 @@ from os import getenv
 def login():
     """Session Authentication route"""
     email = request.form.get('email')
-    if not email or email == "":
+    if not email:
         return jsonify({"error": "email missing"}), 400
 
     password = request.form.get('password')
-    if not password or password == "":
+    if not password:
         return jsonify({"error": "password missing"}), 400
 
     users = User.search({'email': email})
-    if not users or users == []:
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
 
-    user: User = users[0]
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            cookie_name = os.environ.get("SESSION_NAME")
+            response.set_cookie(cookie_name, session_id)
+            return response
 
-    from api.v1.app import auth
-    session_id = auth.create_session(user.id)
-    response = jsonify(user.to_json())
-    cookie_name = getenv('SESSION_NAME')
-    response.set_cookie(cookie_name, session_id)
-    return response
+    return jsonify({"error": "wrong password"}), 401
