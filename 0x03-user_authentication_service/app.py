e8f5e7d65f11b33f186f54a8c238b3aa7996555b
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-20 18:21:09 +0300

    get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,5 +62,16 @@ def profile():
     abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """reset password token route"""
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": f"{email}", "reset_token": f"{reset_token}"})
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
