2a989f3e7a061ea091d54ba1eaf2fa60928c28d1
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-20 17:40:08 +0300

    log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -45,12 +45,10 @@ def sessions():
 def logout():
     """logout route"""
     session_id = request.cookies.get("session_id")
-    if session_id:
-        user = AUTH.get_user_from_session_id(session_id)
-        if user:
-            AUTH.destroy_session(user.user_id)
-            return redirect("/")
-        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.user_id)
+        return redirect("/")
     abort(403)
 
 
