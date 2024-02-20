72f980187997c549877886db66317538a1b63dc0
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-20 18:38:30 +0300

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,7 +97,8 @@ class Auth:
             hashed_password = _hash_password(password)
             self._db.update_user(
                 user.id,
-                {"hashed_password": hashed_password, "reset_token": None}
+                hashed_password=hashed_password,
+                reset_token=None
             )
         except NoResultFound:
             raise ValueError
