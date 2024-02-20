90174ca418f21428f577d0adb3ac0469a4ba6efd
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-20 15:55:50 +0300

    log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Flask app Module
 """
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -26,6 +26,17 @@ def users():
         return jsonify({"email": f"{email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def sessions():
+    """sessions route handler"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        AUTH.create_session(email)
+        return jsonify({"email": f"{email}", "message": "logged in"})
+    abort(401)
 
 
 if __name__ == "__main__":
