7c213834ee9eba0fa3abc0aaca0d45d0b009b2be
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-20 18:13:29 +0300

    generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,3 +79,13 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except Exception:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            setattr(user, "reset_token", reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
