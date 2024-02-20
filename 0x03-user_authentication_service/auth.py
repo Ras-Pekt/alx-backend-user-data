29b94cc7ecaec3d3c1fd2c345b6b9653d79852e3
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-20 15:39:02 +0300

    get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,3 +53,13 @@ class Auth:
             )
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """returns a user's session_id"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user_id=user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
