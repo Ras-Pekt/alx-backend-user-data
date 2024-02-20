e13463b4c43af842c0d9f99c1b4c2c9a9e359e6c
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-20 13:06:25 +0300

    user authentication services

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,68 +1,16 @@
 #!/usr/bin/env python3
 """DB module
 """
-# from sqlalchemy import create_engine
-# from sqlalchemy.exc import InvalidRequestError
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.orm.exc import NoResultFound
-# from sqlalchemy.orm.session import Session
-# from user import Base, User
-# import logging
-
-# logging.disable(logging.WARNING)
-
-
-# class DB:
-#     """DB class
-#     """
-
-#     def __init__(self) -> None:
-#         """Initialize a new DB instance
-#         """
-#         self._engine = create_engine("sqlite:///a.db", echo=True)
-#         Base.metadata.drop_all(self._engine)
-#         Base.metadata.create_all(self._engine)
-#         self.__session = None
-
-#     @property
-#     def _session(self) -> Session:
-#         """Memoized session object
-#         """
-#         if self.__session is None:
-#             DBSession = sessionmaker(bind=self._engine)
-#             self.__session = DBSession()
-#         return self.__session
-
-#     def add_user(self, email: str, hashed_password: str) -> User:
-#         """saves a new user to the database"""
-#         new_user = User(email=email, hashed_password=hashed_password)
-#         self._session.add(new_user)
-#         self._session.commit()
-#         return new_user
-
-#     def find_user_by(self, **kwargs: dict) -> User:
-#         """returns the first row found"""
-#         if kwargs:
-#             user = self._session.query(User).filter_by(**kwargs).first()
-#             if user:
-#                 return user
-#             else:
-#                 raise NoResultFound
-#         else:
-#             raise InvalidRequestError
-        
-"""DB module
-"""
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-from user import User
+from sqlalchemy.orm.session import Session
+from user import Base, User
+import logging
 
-from user import Base
+logging.disable(logging.WARNING)
 
 
 class DB:
@@ -72,7 +20,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -87,21 +35,20 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Saves a user to the database
-        """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        """saves a new user to the database"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+        return new_user
 
     def find_user_by(self, **kwargs: dict) -> User:
-        """
-        searches for a user by attributes in key word arguments
-        """
-        if not kwargs:
-            raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if not user:
-            raise NoResultFound
-        return user
+        """returns the first row found"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as e:
+            raise e
