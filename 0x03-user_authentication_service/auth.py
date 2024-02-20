7dc8a5f842902c457fdce999fe89903969a2385b
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-02-20 14:19:54 +0300

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,8 +4,33 @@ a method that takes in a password string arguments
 and returns bytes
 """
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """hashes a password"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        takes mandatory email and password
+        and return a User object
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(
+                email=email,
+                hashed_password=_hash_password(password)
+            )
